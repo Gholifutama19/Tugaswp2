c2a53d0e6ea67ddf60a47d73d57bb1b95d82f429
Gholifutama19 <91356564+Gholifutama19@users.noreply.github.com>
2021-10-01 14:13:22 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Gholif</li>
+ 		<li>Nama Belakang : Praditya</li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jln.Mawar Melati Semuanya Indah No.90</li>
+ 	</ul>
+ 
+ 	<h4>Tempat Lahir</h4>
+ 	<ul type="none">
+ 		<li>Jakarta</li>
+ 	</ul>
+ 	<h4>Olahraga Favorite</h4>
+ 	<ul type="square">
+ 		<li>Volly</li>
+ 		<li>Renang</li>
+ 	</ul>
+</section>
